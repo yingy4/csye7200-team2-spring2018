49ca11854d648b6c878ee839d55f885de77613c5
RohanJahagirdar <rohan.s.jahagirdar@gmail.com>
2018-04-13 20:26:36 -0400

    Added French and German languages to SWRemover

--- src/main/scala/DataTransform/SWRemover.scala
+++ src/main/scala/DataTransform/SWRemover.scala
@@ -7,10 +7,17 @@ object SWRemover {
 
   def removeStopWords(df:DataFrame): DataFrame = {
 
+    val english = StopWordsRemover.loadDefaultStopWords("english")
+    val german = StopWordsRemover.loadDefaultStopWords("german")
+    val french = StopWordsRemover.loadDefaultStopWords("french")
+
+    val stop_words = english ++ german ++ french
+
+
     val remover = new StopWordsRemover()
       .setInputCol("tokenized_words")
       .setOutputCol("filtered lyrics")
-        .setStopWords(StopWordsRemover.loadDefaultStopWords("english"))
+        .setStopWords(stop_words)
     val output = remover.transform(df)
     output
   }
