310cccb1801042b9119851cb2f0612c16e3c5c55
chugh.k <chugh.k@husky.neu.edu>
2018-04-15 23:18:13 -0400

    added feature Rhyme Scheme

---
+++ src/main/scala/FeatureExtraction/RhymeScheme.scala
@@ -0,0 +1,97 @@
+package FeatureExtraction
+
+import org.apache.spark.rdd.RDD
+import Utility._
+import scala.util.Try
+import Stream._
+import scala.collection.immutable.ListMap
+import com.mashape.unirest.http.HttpResponse
+import com.mashape.unirest.http.JsonNode
+import com.mashape.unirest.http.Unirest
+
+object RhymeScheme {
+
+  type inType = RDD[Tuple6[Try[Int], Try[String], Try[Int], Try[String], Try[String], Try[String]]]
+  type outType = RDD[Tuple7[Try[Int], Try[String], Try[Int], Try[String], Try[String], Try[String], Try[String]]]
+
+  def transformWithRhymeScheme(inRDD : inType) : outType = {
+
+    inRDD.map( rt => {
+
+      val itlt = for ( t <- rt._6) yield {
+
+        val listLines = t.replaceAll("[\"]","").trim.replaceAll("[ ]","+").split("\\n").toList
+
+        def getSyllablesPerLine(acc : List[Try[Int]], line : String) : List[Try[Int]] = {
+
+          val syllableTry = Try(Unirest.post(s"https://ipeirotis-readability-metrics.p.mashape.com/getReadabilityMetrics?text=$line").header("X-Mashape-Key", "2DakK9sBl0mshVU2bhO8CGRwQfHAp1G2xevjsnNaIX23Gjdiqc").header("Content-Type", "application/x-www-form-urlencoded").header("Accept", "application/json").asJson.getBody.getObject.get("SYLLABLES").asInstanceOf[Double].toInt)
+
+          val newAcc = acc :+ syllableTry
+
+          newAcc
+        }
+
+        val listSyllables = listLines.foldLeft(List[Try[Int]]())(getSyllablesPerLine);
+
+        listSyllables
+
+      }
+
+      val slt = for (il <- convert(itlt)) yield {
+
+        val min = il.min; val max = il.max; val t1 = min + ((max-min)/3); val t2 = min + ((max-min)*2/3);
+
+        val sl = for (i <- il) yield
+
+          if (i<t1) "s"
+
+          else {
+
+            if (i>t2) "l"
+
+            else "m"
+
+          }
+
+        sl
+
+      }
+
+      val st = for (sl <- slt) yield {
+
+        val txt = sl.mkString
+
+        val N = txt.length
+
+        val s1 = from (0) take (N/2)
+
+        val occList = s1.foldLeft( ListMap[Int, String]() ) ((lm,i) => {
+
+          var s2 = from (i+3)
+
+          if ((i+6) > (N/2)) { s2 = s2 take (N/2 - i -3) } else { s2 = s2 take 6 }
+
+          val lm2 = s2.foldLeft( ListMap[Int, String]() )((a,j) => {
+
+            val subStr = txt.substring(i,j)
+            val occ = searchSubstringOccurences(subStr, txt)
+
+            a + (occ -> subStr)
+
+          })
+
+          lm2 ++ lm
+        })
+
+        val pattern = occList.max._2
+
+        pattern
+
+      }
+
+      ( rt._1, rt._2, rt._3, rt._4, rt._5, rt._6, st )
+
+    })
+
+  }
+}
---
+++ src/main/scala/FeatureExtraction/Utility.scala
@@ -0,0 +1,54 @@
+package FeatureExtraction
+
+import scala.util.Try
+
+object Utility {
+
+  def convert[X](xtlt : Try[List[Try[X]]]) : Try[List[X]] = {
+
+    val xlt = for ( xtl <- xtlt)
+
+      yield for ( xt <- xtl; if (xt.isSuccess) ) yield xt.get
+
+
+    xlt
+
+  }
+
+  //ROBIN-KARP SUBSTRING SEARCH
+  //val pat = "ssml"
+  //val txt = "smlssmllmslmssmlslssmlssml"
+
+  def searchSubstringOccurences(pat : String, txt : String) : Int = {
+
+    val M = pat.length
+    val N = txt.length
+    val d = 3
+    val q = 101
+    import Stream._
+
+
+    val h = (from (0) take (M-1)).foldLeft(1)((h,i) => (h*d)%q)
+
+    val (p,t) = (from (0) take (M)).foldLeft((0,0))((a,i) => {
+      val (p,t) = a
+      ((d*p + pat.charAt(i))%q, (d*t + txt.charAt(i))%q)
+    })
+
+    val (_,_,occurences) = (from (0) take (N-M+1)).foldLeft((t,p,0))((a,i) => {
+
+      val (t,p,o) = a
+
+      def checkMatch : Int = if (t == p && txt.substring(i,i+M).equals(pat)) 1 else 0
+
+      //println( (i+M)%N + " " + (((d*(t - txt.charAt(i)*h) + txt.charAt((i+M)%N))%q)+q) + " " + txt.substring(i,i+M))
+
+      ( (((d*(t - txt.charAt(i)*h) + txt.charAt((i+M)%N))%q)+q)%q, p, o + checkMatch)
+
+    })
+
+    occurences
+
+  }
+
+}
